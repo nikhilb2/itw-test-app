a19ae790e70e0f5d8d1f30085d93bc9e09e67069
Arthur jl DUPONT <arthur.jl.dupont@protonmail.com>
2021-10-22 15:10:55 +0200

    remove discount in basket

--- backend/src/database/entities/Basket.ts
+++ backend/src/database/entities/Basket.ts
@@ -5,15 +5,11 @@ import { v4 } from 'uuid';
 export class BasketEntity {
   constructor(init: { id?: string; discount: number; payed: boolean }) {
     this.id = init.id ?? v4();
-    this.discount = init.discount ?? 0;
     this.payed = init.payed ?? false;
   }
 
   @PrimaryKey({ columnType: 'uuid', defaultRaw: 'gen_random_uuid()' })
   id: string;
-
-  @Property({ columnType: 'integer', default: 0 })
-  discount: number;
 
   @Property({ columnType: 'boolean', default: false })
   payed: boolean;
