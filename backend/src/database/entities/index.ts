f0c2119b4708d4a63027fc1ee72d4449b66970ec
Arthur jl DUPONT <arthur.jl.dupont@protonmail.com>
2021-10-26 14:54:09 +0200

    add basket tables to index

--- backend/src/database/entities/index.ts
+++ backend/src/database/entities/index.ts
@@ -1,7 +1,22 @@
 import { DishEntity, DishType } from './Dish';
 import { DishIngredientEntity } from './DishIngredient';
 import { IngredientEntity } from './Ingredient';
+import { BasketEntity } from './Basket';
+import { BasketDishEntity } from './BasketDish';
 
-export const entities = [DishEntity, IngredientEntity, DishIngredientEntity];
+export const entities = [
+  DishEntity,
+  IngredientEntity,
+  DishIngredientEntity,
+  BasketEntity,
+  BasketDishEntity,
+];
 
-export { DishEntity, IngredientEntity, DishIngredientEntity, DishType };
+export {
+  DishEntity,
+  IngredientEntity,
+  DishIngredientEntity,
+  DishType,
+  BasketEntity,
+  BasketDishEntity,
+};
